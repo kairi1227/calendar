a53a43534322cc868531f31e5273bccf6e7548a7
xuqiang <xuqiang@wafersystems.com>
2017-09-12 14:42:18 +0800

    edit date value of week view

--- src/week/WeekPanel.jsx
+++ src/week/WeekPanel.jsx
@@ -32,33 +32,43 @@ export default createReactClass({
     const { isShowAllDay, headerRender, timeFormat, contentRender, locale } = props;
     const prefixCls = this.prefixCls;
     const day = value.clone();
+    const header = value.clone();
+    const allDay = value.clone();
     return (
       <div className={prefixCls} style={props.style}>
         <div className={`${prefixCls}-header`}>
           <div/>
             {Array.from({ length: 7 }, (v, i) => i).map(i =>
               <div key={i} className={value.day() === (i + 1) && 'today' || ''}>{headerRender
-              && headerRender(day.startOf('week').add(i, 'day'))
-              || day.startOf('week').add(i, 'day').format(locale.weekHeaderFormat)}</div>
+              && headerRender(header.startOf('week').add(i, 'day'), locale)
+              || header.startOf('week').add(i, 'day').format(locale.weekHeaderFormat)}</div>
             )}
         </div>
         <div className={`${prefixCls}-event`}>
           {isShowAllDay && <div className={'all-day'}>
             <div>all day</div>
             {Array.from({ length: 7 }, (v, i) => i).map(i =>
-              <div key={i} className={day.day() === (i + 1) && 'today' || ''}>
-                {contentRender(value)}
+              <div key={i} className={value.day() === (i + 1) && 'today' || ''}>
+                {contentRender
+                && contentRender(allDay.startOf('week').add(i, 'day'), locale)
+                || null}
               </div>
             )}
           </div>}
           {
             Array.from({ length: 24 }, (v, i) => i).map(i => {
+              const cellDay = value.clone();
               return (
                 <div className={'event-tr'} key={i}>
                   <div>{day.startOf('day').add(i, 'hour').format(timeFormat)}</div>
                     {Array.from({ length: 7 }, (v, l) => l).map(d =>
                       <div key={d} className={value.day() === (d + 1) && 'today' || ''}>
-                        {contentRender && contentRender(value) || ''}
+                        {contentRender && contentRender(
+                          cellDay.startOf('week')
+                          .add(d, 'day')
+                          .add(i, 'hour'),
+                          locale)
+                        || null}
                       </div>
                     )}
                 </div>
